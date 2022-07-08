fac284c1e40787b6143167019f6c9cc4b8859af6
JanPuncuh <jan.puncuh1@gmail.com>
2022-07-08 22:17:11 +0200

    skrije/pokaže geslo

--- app/src/main/java/com/example/janinfinum/LoginActivity.kt
+++ app/src/main/java/com/example/janinfinum/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.janinfinum
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.example.janinfinum.databinding.ActivityLoginBinding
+import android.view.MotionEvent
+import android.view.View
+
+import android.view.View.OnTouchListener
+import android.text.method.PasswordTransformationMethod
+
+
+
 
 
 lateinit var binding: ActivityLoginBinding
@@ -14,11 +23,13 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var showPassword = false
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            
+
             // Create the text message with a string.
             val substring = binding.editTextEmailAddress.text.toString().substringBefore('@')
 
@@ -47,6 +58,35 @@ class LoginActivity : AppCompatActivity() {
             //login button dela, če je pravilen mail in geslo
             binding.button.isEnabled = validateLogin(binding.editTextEmailAddress.text.toString(), binding.editTextPassword.text.toString())
         }
+
+        binding.editTextPassword.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= binding.editTextPassword.right - binding.editTextPassword.compoundDrawables[DRAWABLE_RIGHT].bounds.width()) {
+                    //your action here
+
+                    //geslo je prikazano, skrije ga
+                    if (showPassword) {
+                        Log.i("TEST", "test")
+                        binding.editTextPassword.transformationMethod = PasswordTransformationMethod() //hide the password from the edit text
+
+                    }
+                    //geslo je skirto, pokaže ga
+                    else {
+                        Log.i("TEST", "test2")
+                        binding.editTextPassword.transformationMethod = null; // another option show the password from the edit text
+                    }
+                    showPassword = !showPassword
+
+                    return@OnTouchListener true
+                }
+            }
+            return@OnTouchListener false
+        })
+
     }
 
     private fun emailValidate(email: String): Boolean {
