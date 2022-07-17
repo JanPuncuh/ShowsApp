f14815e034a3a6eec64a5ffcc503d7db36bd653b
JanPuncuh <jan.puncuh1@gmail.com>
2022-07-17 20:17:23 +0200

    Show now has UUID

--- app/src/main/java/com/example/janinfinum/Show.kt
+++ app/src/main/java/com/example/janinfinum/Show.kt
@@ -1,9 +1,12 @@
 package com.example.janinfinum
 
 import androidx.annotation.DrawableRes
+import java.util.*
+import java.util.UUID.randomUUID
 
 data class Show(
     val title: String,
     val description: String,
-    @DrawableRes val imageResourceId: Int
+    @DrawableRes val imageResourceId: Int,
+    val ID: UUID = randomUUID()
 )
