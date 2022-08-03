577632b922d11d20fa246ce37e3f894fe9747d31
JanPuncuh <jan.puncuh1@gmail.com>
2022-08-03 21:54:40 +0200

    splash screen

--- app/src/main/java/com/example/janinfinum/MainActivity.kt
+++ app/src/main/java/com/example/janinfinum/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.janinfinum
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.janinfinum.databinding.ActivityMainBinding
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
